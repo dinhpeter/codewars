75b292c26d40a947cf2daac906cd22bf01d74655
Peter Dinh <dinh_peter@yahoo.com>
2022-12-28 23:19:19 -0800

    day 34

---
+++ 8kyu/create-first-JS-function.js
@@ -0,0 +1,12 @@
+/*
+
+In JavaScript, your code is running in a function, let us step by step complete your first JS function.
+
+*/
+
+function helloWorld() {
+    let str = 'Hello World!'
+    console.log(str)
+  }
+
+  
