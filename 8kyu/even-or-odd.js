acec0ab7a08562482e99f563a2060a2dedf8fcdd
Peter Dinh <dinh_peter@yahoo.com>
2023-01-03 20:53:05 -0800

    day 39

---
+++ 8kyu/even-or-odd.js
@@ -0,0 +1,22 @@
+/*
+
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+*/
+
+function evenOrOdd(number) {
+    if(number%2 == 0){
+      return "Even"
+    }else{
+      return "Odd"
+    }
+  }
+
+
+/*
+
+function even_or_odd(number) {
+  return number % 2 ? "Odd" : "Even"
+}
+
+*/
