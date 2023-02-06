f13130b4bfbc6bef430302843f27b254f975ccad
Peter Dinh <dinh_peter@yahoo.com>
2023-02-06 13:36:11 -0800

    day 72

---
+++ 8kyu/find-multiples-of-a-number.js
@@ -0,0 +1,34 @@
+/*
+
+In this simple exercise, you will build a program that takes a value, integer , and returns a list of its multiples up to another value, limit . If limit is a multiple of integer, it should be included as well. There will only ever be positive integers passed into the function, not consisting of 0. The limit will always be higher than the base.
+
+For example, if the parameters passed are (2, 6), the function should return [2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6.
+
+*/
+
+function findMultiples(integer, limit) {
+    let current = 0
+    let arr = []
+    for(let i=0; current<=limit; i++){
+      current = integer*(i+1)
+      if(integer*(i+1)<=limit){
+        arr.push(integer*(i+1))
+      }
+      
+    }
+    return arr
+  }
+  
+
+  /*
+
+  function findMultiples(int,limit){
+  let result = []
+  
+  for (let i = int; i<=limit ; i+=int)
+    result.push(i)
+    
+  return result
+}
+
+*/
