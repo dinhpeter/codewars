12ab478982f0d8557b6a14b5d2e27a6428fdbc1a
Peter Dinh <dinh_peter@yahoo.com>
2023-05-26 11:45:44 -0700

    day 172

---
+++ 8kyu/contamination-string.js
@@ -0,0 +1,30 @@
+/*
+An AI has infected a text with a character!!
+
+This text is now fully mutated to this character.
+
+If the text or the character are empty, return an empty string.
+There will never be a case when both are empty as nothing is going on!!
+
+Note: The character is a string of length 1 or an empty string.
+
+Example
+text before = "abc"
+character   = "z"
+text after  = "zzz"
+*/
+
+function contamination(text, char){
+    let test = ''
+    for(let i=0; i<text.length; i++){
+      test += char
+    }
+    return test
+  }
+
+  /*
+  function contamination(text, char){
+    return char.repeat(text.length)
+  }
+  */
+ 
