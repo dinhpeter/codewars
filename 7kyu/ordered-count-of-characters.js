499cca27406e16d472c73b8a08adc4c148b13d4e
Peter Dinh <dinh_peter@yahoo.com>
2023-05-30 18:52:09 -0700

    day 175

---
+++ 7kyu/ordered-count-of-characters.js
@@ -0,0 +1,33 @@
+/*
+Count the number of occurrences of each character and return it as a (list of tuples) in order of appearance. For empty output return (an empty list).
+
+Consult the solution set-up for the exact data structure implementation depending on your language.
+
+Example:
+
+orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
+*/
+
+const orderedCount = function (text) {
+    let ans = []
+    
+    for(let i=0; i<text.length; i++){
+      let count = 0;
+      
+      for(let j=0; j<ans.length; j++){
+        if(ans[j][0] === text[i]){
+          count++;
+        }
+      }
+      
+      if (count === 0){
+        for (let j=i; j<text.length; j++){
+          if(text[j] === text[i]){
+            count++;
+          }
+        }
+        ans.push([text[i], count]);
+      }
+    }
+    return ans
+  }
